b679797f0accf27181969056fe06bdc844ddc1ab
Nameless1004 <jjho1029@gmail.com>
2024-11-21 10:27:58 +0900

    [update] 엔티티 수정

--- src/main/java/org/sparta/batch/domain/waiting/entity/DailyWaitingStatistics.java
+++ src/main/java/org/sparta/batch/domain/waiting/entity/DailyWaitingStatistics.java
@@ -33,21 +33,21 @@ public class DailyWaitingStatistics {
 
 
     private double completedAverageWaitingTime = 0.0;
-    private double canceledAverageWaitTime = 0.0;
+    private double canceledAverageWaitingTime = 0.0;
 
     private double cancellationRate = 0.0; // 00%
 
     private LocalDateTime createdAt;
 
     @Builder
-    public DailyWaitingStatistics(Store store, LocalDate date, int totalWaitingCount, int completedCount, int canceledCount, double completedAverageWaitingTime, double canceledAverageWaitTime, double cancellationRate, LocalDateTime createdAt) {
+    public DailyWaitingStatistics(Store store, LocalDate date, int totalWaitingCount, int completedCount, int canceledCount, double completedAverageWaitingTime, double canceledAverageWaitingTime, double cancellationRate, LocalDateTime createdAt) {
         this.store = store;
         this.date = date;
         this.totalWaitingCount = totalWaitingCount;
         this.completedCount = completedCount;
         this.canceledCount = canceledCount;
         this.completedAverageWaitingTime = completedAverageWaitingTime;
-        this.canceledAverageWaitTime = canceledAverageWaitTime;
+        this.canceledAverageWaitingTime = canceledAverageWaitingTime;
         this.cancellationRate = cancellationRate;
         this.createdAt = LocalDateTime.now();
     }
@@ -59,7 +59,7 @@ public class DailyWaitingStatistics {
         this.completedCount = dto.getCompletedCount();
         this.canceledCount = dto.getCanceledCount();
         this.completedAverageWaitingTime = BigDecimal.valueOf(dto.getCompletedAverageWaitingTime()).setScale(1, RoundingMode.HALF_UP).doubleValue();
-        this.canceledAverageWaitTime =  BigDecimal.valueOf(dto.getCanceledAverageWaitTime()).setScale(1, RoundingMode.HALF_UP).doubleValue();
+        this.canceledAverageWaitingTime =  BigDecimal.valueOf(dto.getCanceledAverageWaitTime()).setScale(1, RoundingMode.HALF_UP).doubleValue();
         this.cancellationRate = this.totalWaitingCount > 0
                 ?  BigDecimal.valueOf((double) this.canceledCount * 100 / this.totalWaitingCount).setScale(2, RoundingMode.HALF_UP).doubleValue()
                 : 0;
